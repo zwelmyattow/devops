9410638034f18ea47f54297bf2d97bcd302b05dc
zwelmyattow <zwelmyattow2.0@gmail.com>
2026-09-14 16:02:56 +0630

    Complete employee SQL query and bump version

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,7 +4,15 @@ import java.sql.*;
 
 public class App
 {
-    public static void main(String[] args)
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
     {
         try
         {
@@ -17,9 +25,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -30,9 +36,6 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -45,7 +48,13 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -58,5 +67,86 @@ public class App
                 System.out.println("Error closing connection to database");
             }
         }
+    }
+
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement with JOINs
+            String strSelect =
+                    "SELECT emp.emp_no, emp.first_name, emp.last_name, titles.title, salaries.salary, departments.dept_name, manager.first_name AS manager_first, manager.last_name AS manager_last "
+                            + "FROM employees emp "
+                            + "JOIN titles ON (emp.emp_no = titles.emp_no) "
+                            + "JOIN salaries ON (emp.emp_no = salaries.emp_no) "
+                            + "JOIN dept_emp ON (emp.emp_no = dept_emp.emp_no) "
+                            + "JOIN departments ON (dept_emp.dept_no = departments.dept_no) "
+                            + "JOIN dept_manager ON (departments.dept_no = dept_manager.dept_no) "
+                            + "JOIN employees manager ON (dept_manager.emp_no = manager.emp_no) "
+                            + "WHERE emp.emp_no = " + ID + " "
+                            + "AND titles.to_date = '9999-01-01' "
+                            + "AND salaries.to_date = '9999-01-01' "
+                            + "AND dept_emp.to_date = '9999-01-01' "
+                            + "AND dept_manager.to_date = '9999-01-01'";
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Extract employee information
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                emp.title = rset.getString("title");
+                emp.salary = rset.getInt("salary");
+                emp.dept_name = rset.getString("dept_name");
+                emp.manager = rset.getString("manager_first") + " " + rset.getString("manager_last");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+
+        // Display results
+        a.displayEmployee(emp);
+
+        // Disconnect from database
+        a.disconnect();
     }
 }
---
+++ src/main/java/com/napier/sem/Employee.java
@@ -0,0 +1,15 @@
+package com.napier.sem;
+
+/**
+ * Represents an employee
+ */
+public class Employee
+{
+    public int emp_no;
+    public String first_name;
+    public String last_name;
+    public String title;
+    public int salary;
+    public String dept_name;
+    public String manager;
+}
